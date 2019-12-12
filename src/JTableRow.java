361bae09a8f3656ee341d6e043d546397995b169
bob2708 <vovanerukhov@mail.ru>
2019-12-12 19:20:14 +0300

    editting table

--- src/JTableRow.java
+++ src/JTableRow.java
@@ -1,42 +1,134 @@
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class JTableRow {
-
     public JTableRow() {
 
-        // create JFrame and JTable
-        JFrame frame = new JFrame();
-        JTable table = new JTable();
 
-        // create a table model and set a Column Identifiers to this model
-        Object[] columns = {"Song name","Voices"};
-        DefaultTableModel model = new DefaultTableModel();
-        model.setColumnIdentifiers(columns);
+            JFrame frame = new JFrame();
+            JTable table = new JTable();
 
-        // set the model to the table
-        table.setModel(model);
+            // create a table model and set a Column Identifiers to this model
+            Object[] columns = {"Name", "Voices"};
+            DefaultTableModel model = new DefaultTableModel();
+            model.setColumnIdentifiers(columns);
 
-        // create JScrollPane
-        JScrollPane pane = new JScrollPane(table);
-        pane.setBounds(5, 5, 300, 150);
+            // set the model to the table
+            table.setModel(model);
 
-        frame.setLayout(null);
+            // create JTextFields
+            JTextField textName = new JTextField();
+            JTextField textVoices = new JTextField();
 
-        frame.add(pane);
+            // create JButtons
+            JButton btnAdd = new JButton("Add");
+            JButton btnDelete = new JButton("Delete");
+            JButton btnUpdate = new JButton("Update");
 
-        // create an array of objects to set the row data
-        Object[] row = new Object[2];
-        for(Song song : ConcertSystem.login.songs) {
-            row[0] = song.getName();
-            row[1] = song.getVoices();
-            model.addRow(row);
-        }
+            textName.setBounds(20, 220, 100, 25);
+            textVoices.setBounds(20, 250, 100, 25);
 
-        frame.setSize(330,200);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+            btnAdd.setBounds(150, 220, 100, 25);
+            btnUpdate.setBounds(150, 265, 100, 25);
+            btnDelete.setBounds(150, 310, 100, 25);
+
+            // create JScrollPane
+            JScrollPane pane = new JScrollPane(table);
+            pane.setBounds(0, 0, 880, 200);
+
+            frame.setLayout(null);
+
+            frame.add(pane);
+
+            // add JTextFields to the jframe
+            frame.add(textName);
+            frame.add(textVoices);
+
+            // add JButtons to the jframe
+            frame.add(btnAdd);
+            frame.add(btnDelete);
+            frame.add(btnUpdate);
+
+            // create an array of objects to set the row data
+            Object[] row = new Object[2];
+
+            for (Song song : ConcertSystem.login.songs) {
+                row[0] = song.getName();
+                row[1] = song.getVoices();
+                model.addRow(row);
+            }
+            // button add row
+            btnAdd.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) throws NumberFormatException {
+                    try {
+                        row[0] = textName.getText();
+                        row[1] = textVoices.getText();
+                        // add row to the model
+                        ConcertSystem.login.songs.add(new Song(textName.getText(), Integer.parseInt(textVoices.getText())));
+                        model.addRow(row);
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Неверный формат ввода","Ошибка",JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
+
+            // button remove row
+            btnDelete.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // i = the index of the selected row
+                    int i = table.getSelectedRow();
+                    if (i >= 0) {
+                        // remove a row from jtable
+                        model.removeRow(i);
+                        ConcertSystem.login.songs.remove(i);
+                    } else {
+                        System.out.println("Delete Error");
+                    }
+                }
+            });
+
+            // get selected row data From table to textfields
+            table.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    // i = the index of the selected row
+                    int i = table.getSelectedRow();
+                    textName.setText(model.getValueAt(i, 0).toString());
+                    textVoices.setText(model.getValueAt(i, 1).toString());
+                }
+            });
+
+            // button update row
+            btnUpdate.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) throws NumberFormatException {
+                    // i = the index of the selected row
+                    int i = table.getSelectedRow();
+                    try {
+                        if (i >= 0) {
+                            ConcertSystem.login.songs.get(i).setVoices(Integer.parseInt(textVoices.getText()));
+                            ConcertSystem.login.songs.get(i).setName(textName.getText());
+                            model.setValueAt(textName.getText(), i, 0);
+                            model.setValueAt(textVoices.getText(), i, 1);
+                        } else {
+                            System.out.println("Update Error");
+                        }
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Неверный формат ввода","Ошибка",JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
+
+            frame.setSize(900, 400);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
     }
 }
